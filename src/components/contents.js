0633ccc00263e946753c89a106ff14cfec2e0546
CvijaI <123738999+CvijaI@users.noreply.github.com>
2023-10-06 16:40:21 +0100

    commentsAdded contents.js

--- src/components/contents.js
+++ src/components/contents.js
@@ -1,10 +1,13 @@
+//FUNCTIONAL COMPONENT
+//definition of component Content
 function Content(){
     return(
         <div>
             <h1>Hello World!</h1>
+        //showing current time
             <h2>It is {new Date().toLocaleTimeString()}.</h2>
         </div>
     )
 }
-
+//exporting the Content
 export default Content;
